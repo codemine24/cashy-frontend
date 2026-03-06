3d68debfe539e1c8d5471cc780e2234f15e38f68
codemine24 <codemine24@gmail.com>
2026-03-07 01:48:37 +0600

    subscription.ts basic version implemented

---
+++ api/subscription.ts
@@ -0,0 +1,38 @@
+import apiClient from "@/lib/api-client";
+import { throwApiError } from "@/utils/throw-api-error";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export type CreateSubscriptionPayload = {
+  plan: "LIFETIME";
+  price: number;
+  purchase_token: string;
+  product_id: string;
+  package_name?: string;
+};
+
+export const useCreateSubscription = () => {
+  return useMutation({
+    mutationFn: async (payload: CreateSubscriptionPayload) => {
+      try {
+        const response = await apiClient.post("/subscriptions", payload);
+        return response.data;
+      } catch (error) {
+        throwApiError(error);
+      }
+    },
+  });
+};
+
+export const useGetMySubscription = () => {
+  return useQuery({
+    queryKey: ["my-subscription"],
+    queryFn: async () => {
+      try {
+        const response = await apiClient.get("/subscriptions/my-subscription");
+        return response.data?.data;
+      } catch (error) {
+        throwApiError(error);
+      }
+    },
+  });
+};
