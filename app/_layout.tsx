a684d5a75bbdedf656b0481d46c587edbcb12374
codemine <codemine24@gmail.com>
2026-03-18 02:13:24 +0600

    tab screen swap added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { RootNavigator } from "@/components/root-navigator";
 import { ThemeProvider } from "@/context/theme-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "@/i18n";
 import '@/styles/global.css';
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <RootNavigator />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider>
+        <RootNavigator />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
