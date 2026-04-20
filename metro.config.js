a67b0bc4e68693abac170073ecea22d71ea98e91
codemine24 <codemine24@gmail.com>
2026-04-21 02:41:00 +0600

    update

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,19 @@
+const path = require("path");
+process.chdir(__dirname);
+
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+config.transformer.babelTransformerPath =
+  require.resolve("react-native-svg-transformer");
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => ext !== "svg",
+);
+config.resolver.sourceExts = [...config.resolver.sourceExts, "svg"];
 
-module.exports = withNativeWind(config, { input: './styles/global.css' })
+module.exports = withNativeWind(config, {
+  input: path.join(__dirname, "styles/global.css"),
+  config: path.join(__dirname, "tailwind.config.js"),
+});
